86cce3dc93c09110eb8532c24664b3f36722ff70
Rj Solucky <rjsolucky9999@gmail.com>
2024-05-04 19:41:31 +0530

    Update DiyaMakingMachine.tsx

--- src/app/components/product/DiyaMakingMachine.tsx
+++ src/app/components/product/DiyaMakingMachine.tsx
@@ -18,194 +18,113 @@ export const DiyaMakingMachine = () => {
               </div>
               <div className="col-span-12 lg:col-span-6">
                 <div>
-                  <h1 className="text-2xl leading-tight font-medium mb-4">
-                    Diya Making Machine
+                <h1 className="text-2xl leading-none font-bold mb-4">
+                Diya Making Machine
                   </h1>
                 </div>
                 <hr className="dark:border-slate-700 my-6" />
                 <div>
-                  <h2 className="text-lg leading-tight font-medium mb-4">
+                  <h2 className="text-start leading-tight font-medium">
                     This Multi Purpose Manual Diya Making Machine is very useful
                     to make Diyas. By using Different Die (Mold) it can Produce
                     : Deepak/Diya, Kullad, Kitkat fuse, Ceramic foundry filters
                     etc.
                   </h2>
                 </div>
-                <div className="text-lg leading-tight font-medium mb-4">
-                  <h1>Specification</h1>
-                </div>
-                <section
-  className="ezy__epspecification3 light py-14 md:py-24 bg-white dark:bg-[#0b1727] text-zinc-900 dark:text-white relative overflow-hidden z-10"
->
-  <div className="container px-4 mx-auto">
-    <div className="flex flex-col max-w-3xl justify-center mx-auto">
-      <h5 className="text-2xl font-bold leading-none mb-8">
-        Product Specification
-      </h5>
+                <section className="ezy__epspecification3 light py-4 md:py-14 bg-white dark:bg-[#0b1727] text-zinc-900 dark:text-white relative overflow-hidden z-10">
+                  <div className="container px-4 mx-auto">
+                    <div className="flex flex-col max-w-3xl justify-center mx-auto">
+                      <h5 className="text-2xl font-bold leading-none mb-8">
+                        Product Specification
+                      </h5>
 
-      <table>
-        <tbody>
-          <tr className="border-b dark:border-slate-700 mt-2">
-            <td
-              className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20"
-            >
-              Brand Name
-            </td>
+                      <table>
+                        <tbody>
+                          <tr className="border-b dark:border-slate-700 mt-2">
+                            <td className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20">
+                            Product Type
+                            </td>
 
-            <th className="py-4 text-start pl-2 md:pl-8">T-WINNERWater</th>
-          </tr>
-          <tr className="border-b dark:border-slate-700 mt-2">
-            <td
-              className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20"
-            >
-              Resistance Depth
-            </td>
+                            <th className="py-4 text-start pl-2 md:pl-8">
+                            Diya Making Machine
+                            </th>
+                          </tr>
+                          <tr className="border-b dark:border-slate-700 mt-2">
+                            <td className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20">
+                            Material
+                            </td>
 
-            <th className="py-4 text-start pl-2 md:pl-8">No waterproof</th>
-          </tr>
-          <tr className="border-b dark:border-slate-700 mt-2">
-            <td
-              className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20"
-            >
-              Origin
-            </td>
+                            <th className="py-4 text-start pl-2 md:pl-8">
+                            Mild Steel
+                            </th>
+                          </tr>
+                          <tr className="border-b dark:border-slate-700 mt-2">
+                            <td className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20">
+                            Machine Height
+                            </td>
 
-            <th className="py-4 text-start pl-2 md:pl-8">Origin</th>
-          </tr>
-          <tr className="border-b dark:border-slate-700 mt-2">
-            <td
-              className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20"
-            >
-              Clasp Type
-            </td>
+                            <th className="py-4 text-start pl-2 md:pl-8">
+                            4 feet, 5 feet, 6 feet, 7 feet
+                            </th>
+                          </tr>
+                          <tr className="border-b dark:border-slate-700 mt-2">
+                            <td className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20">
+                            Capacity (per Day)
+                            </td>
 
-            <th className="py-4 text-start pl-2 md:pl-8">Bracelet Clasp</th>
-          </tr>
-          <tr className="border-b dark:border-slate-700 mt-2">
-            <td
-              className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20"
-            >
-              Style
-            </td>
+                            <th className="py-4 text-start pl-2 md:pl-8">
+                            9000-10000 Pcs.
+                            </th>
+                          </tr>
+                          <tr className="border-b dark:border-slate-700 mt-2">
+                            <td className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20">
+                            Max Force / Load
+                            </td>
 
-            <th className="py-4 text-start pl-2 md:pl-8">Fashion & Casual</th>
-          </tr>
-          <tr className="border-b dark:border-slate-700 mt-2">
-            <td
-              className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20"
-            >
-              Movement
-            </td>
+                            <th className="py-4 text-start pl-2 md:pl-8">
+                            0-30 ton
+                            </th>
+                          </tr>
+                          <tr className="border-b dark:border-slate-700 mt-2">
+                            <td className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20">
+                            Type of Press
+                            </td>
 
-            <th className="py-4 text-start pl-2 md:pl-8">
-              Mechanical Hand Wind
-            </th>
-          </tr>
-          <tr className="border-b dark:border-slate-700 mt-2">
-            <td
-              className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20"
-            >
-              HDMI Resolution
-            </td>
+                            <th className="py-4 text-start pl-2 md:pl-8">
+                            Assembly Press
+                            </th>
+                          </tr>
+                          <tr className="border-b dark:border-slate-700 mt-2">
+                            <td className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20">
+                            Usage
+                            </td>
 
-            <th className="py-4 text-start pl-2 md:pl-8">
-              3840*2160 @ 30Hz <br />1920*1080 @ 60Hz
-            </th>
-          </tr>
-          <tr className="border-b dark:border-slate-700 mt-2">
-            <td
-              className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20"
-            >
-              Case Material
-            </td>
+                            <th className="py-4 text-start pl-2 md:pl-8">
+                            Diya Making & Kitkat Fuse Making
+                            </th>
+                          </tr>
+                          <tr className="border-b dark:border-slate-700 mt-2">
+                            <td className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20">
+                            Material
+                            </td>
 
-            <th className="py-4 text-start pl-2 md:pl-8">Stainless steel</th>
-          </tr>
-          <tr className="border-b dark:border-slate-700 mt-2">
-            <td
-              className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20"
-            >
-              Feature
-            </td>
-
-            <th className="py-4 text-start pl-2 md:pl-8">Luminous hands</th>
-          </tr>
-          <tr className="border-b dark:border-slate-700 mt-2">
-            <td
-              className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20"
-            >
-              Model Number
-            </td>
-
-            <th className="py-4 text-start pl-2 md:pl-8">
-              USB-C multi-function adapter
-            </th>
-          </tr>
-          <tr className="border-b dark:border-slate-700 mt-2">
-            <td
-              className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20"
-            >
-              Item Type
-            </td>
-
-            <th className="py-4 text-start pl-2 md:pl-8">
-              Mechanical Wristwatches
-            </th>
-          </tr>
-          <tr className="border-b dark:border-slate-700 mt-2">
-            <td
-              className="px-6 py-4 text-start w-1/4 opacity-75 bg-gray-100 bg-opacity-80 dark:bg-slate-700 dark:bg-opacity-20"
-            >
-              Band Material Type
-            </td>
-
-            <th className="py-4 text-start pl-2 md:pl-8">Stainless steel</th>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
-</section>
-                <div className="flex justify-between text-md leading-tight font-medium mb-2">
-                  <p>Product Type: </p>
-                  <p>Diya Making Machine </p>
-                </div>
-                <div className="flex justify-between  text-md leading-tight font-medium mb-2">
-                  <p>Material: </p>
-                  <p>Mild Steel </p>
-                </div>
-                <div className="flex justify-between  text-md leading-tight font-medium mb-2">
-                  <p>Machine Height: </p>
-                  <p>4 feet, 5 feet, 6 feet, 7 feet</p>
-                </div>
-                <div className="flex justify-between  text-md leading-tight font-medium mb-2">
-                  <p>Capacity: </p>
-                  <p>9000-10000/day</p>
-                </div>
-                <div className="flex justify-between  text-md leading-tight font-medium mb-2">
-                  <p>Max Force or Load : </p>
-                  <p>0-30 ton</p>
-                </div>
-                <div className="flex justify-between  text-md leading-tight font-medium mb-2">
-                  <p>Type of Press: </p>
-                  <p>Assembly Press</p>
-                </div>
-                <div className="flex justify-between  text-md leading-tight font-medium mb-2">
-                  <p>Usage: </p>
-                  <p>Diya Making & Kitkat Fuse Making</p>
-                </div>
-                <div className="flex justify-between  text-md leading-tight font-medium mb-2">
-                  <p>Material: </p>
-                  <p>Iron</p>
-                </div>
+                            <th className="py-4 text-start pl-2 md:pl-8">
+                            Iron
+                            </th>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </section>
                 <div>
-                  <p className=" text-md leading-tight font-medium mb-2">
+                <h2 className="text-start leading-tight">
                     We are a reputed entity, engaged in offering a qualitative
                     array of Heavy Duty Toggle Press Machine.Keeping in mind
                     ever-evolving requirements of our respected clients, we are
                     providing a premium quality range of Clay Diya Making
                     Machine.
-                  </p>
+                  </h2>
                 </div>
               </div>
             </div>
