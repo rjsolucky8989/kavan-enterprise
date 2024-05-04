657ff269d5bf07dd8f25e4b6c7c8a5bd8c5a74d6
akshayp <akshayvpathak@yahoo.com>
2024-05-04 16:21:42 +0530

    fix changes

--- src/app/components/product/DiyaMakingMachine.tsx
+++ src/app/components/product/DiyaMakingMachine.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
+
 export const DiyaMakingMachine = () => {
   return (
     <section className="py-14 md:py-24 bg-white dark:bg-[#0b1727] text-zinc-900 dark:text-white relative overflow-hidden z-10">
@@ -5,7 +8,14 @@ export const DiyaMakingMachine = () => {
         <div className="grid grid-cols-12 gap-6">
           <div className="col-span-12 lg:col-span-12 relative">
             <div className="grid grid-cols-12 gap-6">
-              <div className="col-span-12 lg:col-span-6"></div>
+              <div className="col-span-12 lg:col-span-6">
+                <img
+                  src="/diya-making-machine-1.jpeg"
+                  alt="Diya making machine"
+                  width={500}
+                  height={500}
+                />
+              </div>
               <div className="col-span-12 lg:col-span-6">
                 <div>
                   <h1 className="text-2xl leading-tight font-medium mb-4">
@@ -24,40 +34,40 @@ export const DiyaMakingMachine = () => {
                 <div className="text-lg leading-tight font-medium mb-4">
                   <h1>Specification</h1>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between text-md leading-tight font-medium mb-2">
                   <p>Product Type: </p>
                   <p>Diya Making Machine </p>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between  text-md leading-tight font-medium mb-2">
                   <p>Material: </p>
                   <p>Mild Steel </p>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between  text-md leading-tight font-medium mb-2">
                   <p>Machine Height: </p>
                   <p>4 feet, 5 feet, 6 feet, 7 feet</p>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between  text-md leading-tight font-medium mb-2">
                   <p>Capacity: </p>
                   <p>9000-10000/day</p>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between  text-md leading-tight font-medium mb-2">
                   <p>Max Force or Load : </p>
                   <p>0-30 ton</p>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between  text-md leading-tight font-medium mb-2">
                   <p>Type of Press: </p>
                   <p>Assembly Press</p>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between  text-md leading-tight font-medium mb-2">
                   <p>Usage: </p>
                   <p>Diya Making & Kitkat Fuse Making</p>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between  text-md leading-tight font-medium mb-2">
                   <p>Material: </p>
                   <p>Iron</p>
                 </div>
                 <div>
-                  <p>
+                  <p className=" text-md leading-tight font-medium mb-2">
                     We are a reputed entity, engaged in offering a qualitative
                     array of Heavy Duty Toggle Press Machine.Keeping in mind
                     ever-evolving requirements of our respected clients, we are
--- src/app/product/diya-making-machine/page.tsx
+++ src/app/product/diya-making-machine/page.tsx
@@ -5,13 +5,39 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Diya Making Machine | Kavan Enterprise",
   description:
-    "Discover top-quality Diya making machines from a trusted seller. Shop now for all your Diya machine needs!",
+    "Multi Purpose Manual Diya Making Machine is very useful to make Diyas. By using Different Die (Mold) it can Produce : Deepak/Diya, Kullad, Kitkat fuse, Ceramic foundry filters etc.",
   robots: "index, follow",
   keywords: [
     "Diya Making Machine",
     "Diya Machine",
     "Thangadh Diya Making Machine",
+    "Deepak/Diya",
+    "Kullad",
+    "Kitkat fuse",
+    "Ceramic",
+    "foundry filters",
   ],
+  openGraph: {
+    type: "website",
+    url: "https://kavanenterprise.com/product/diya-making-machine",
+    title: "Diya Making Machine",
+    description:
+      "Multi Purpose Manual Diya Making Machine is very useful to make Diyas. By using Different Die (Mold) it can Produce : Deepak/Diya, Kullad, Kitkat fuse, Ceramic foundry filters etc.",
+    siteName: "Diya Making Machine | Kavan Enterprise",
+    images: [
+      {
+        url: "https://kavanenterprise.com/diya-making-machine-1.jpeg",
+      },
+    ],
+  },
+  twitter: {
+    site: "https://kavanenterprise.com/product/diya-making-machine",
+
+    description:
+      "Multi Purpose Manual Diya Making Machine is very useful to make Diyas. By using Different Die (Mold) it can Produce : Deepak/Diya, Kullad, Kitkat fuse, Ceramic foundry filters etc.",
+    title: "Diya Making Machine",
+    images: "https://kavanenterprise.com/diya-making-machine-1.jpeg",
+  },
 };
 
 const DiyaMakingMachinePage = () => {
